4cf54f895e7619176b68b58d8db28b55632789dd
mydaddy <mydaddy@users.noreply.github.com>
2020-03-31 16:51:53 -0700

    Create interface descriptions based off CDP neighbors

---
+++ cdpdesc.py
@@ -0,0 +1,43 @@
+#
+## Create interface descriptions based off LLDP neighbors
+## show lldp neighbor on switch
+## copy/paste meat of output containing IPs and hostnames to a file
+## run this script against file
+## python desc.py <file>
+#
+
+import sys
+import re
+
+inputFile = sys.argv[1]
+
+try:
+	with open(inputFile) as fObj:
+		cdpNeighbors = fObj.readlines()
+except FileNotFoundError:
+	cdpNeighbors = None
+
+neighbors = []
+alert = 1
+
+for cdpNeighbor in cdpNeighbors:
+	hostname = re.findall(r'^[^SEP][a-zA-Z]+\d{1,}', cdpNeighbor)
+
+	if hostname:
+		description = hostname[0].upper()
+	elif re.search(r'\w\w\w\s\d+\/\d+\/\d+|\w\w\w\s\d+\/\d+', cdpNeighbor):
+		localInterface, remoteInterface = re.findall(r'\w\w\w\s\d+\/\d+\/\d+|\w\w\w\s\d+\/\d+', cdpNeighbor)
+		
+		localInterface, localPort = re.findall(r'^\w\w|\d+\/\d+\/\d+|\d+\/\d+', localInterface)
+		localInterface = f'{localInterface}{localPort}'
+		
+		remoteInterface, remotePort = re.findall(r'^\w\w|\d+\/\d+\/\d+|\d+\/\d+', remoteInterface)
+		description = f'{description} {remoteInterface}{remotePort}'
+		
+		if 'RWA' in description: # normally only RWA's run CDP not LLDP, will combine desc.py and cdpdesc.py to one script
+			neighbors.append({'interface':localInterface, 'alert':alert, 'description':description})
+
+for nei in neighbors:
+	print('interface {0}'.format(nei['interface']))
+	print('description {0}"TYPE":"LAN","MON":1,"ALERT":{1},"DESC":"{2}"{3}'.format('{', nei['alert'], nei['description'], '}'))
+
